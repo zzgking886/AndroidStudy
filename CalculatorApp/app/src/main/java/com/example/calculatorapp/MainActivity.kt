8657c806b75ab5b56b327800be5f65efee819180
zzgking886 <zzgking886@gmail.com>
2021-04-26 19:45:58 +0800

    提交修改布局

--- CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -17,44 +17,47 @@ class MainActivity : AppCompatActivity(){
 
         var onelayout = findViewById<ConstraintLayout>(R.id.testlayout)
 
-        var onebutton = Button(this)
-
-//        onebutton.setLeftTopRightBottom(100,100,100,100)
-//        onebutton.top = 100
-//        onebutton.width = 100
-//        onebutton.height = 100
-        onebutton.tag = 1
-        onebutton.text = "1"
-        onelayout.addView(onebutton)
-        val lp = onebutton.getLayoutParams() as ConstraintLayout.LayoutParams
-        lp.height = 300
-        lp.width = 300
-        lp.topToTop = onelayout.top
-        lp.leftToLeft = onelayout.left
-        lp.topMargin = 200
-        lp.leftMargin = 200
-        onebutton.setLayoutParams(lp)
         var y = 30
-//        for (i in 1..10)
-//        {
-//            println(30 + (i - 1) * 100)
-//            var onebutton = Button(this)
-//            onebutton.left = 100
-//            onebutton.setLeftTopRightBottom(30 + (i - 1) * 100,y,0,0)
-//            onebutton.width = 100
-//            onebutton.height = 100
-//            onebutton.tag = i
-//            onebutton.text = i.toString()
-//            onelayout.addView(onebutton)
-//            if (i % 3 == 0)
-//            {
-//
-//                y += 130
-//            }
-//
-//            onebutton.setOnClickListener {
-//                println(onebutton.getTag())
-//            }
-//        }
+        var origin_x = 30
+        for (i in 1..10)
+        {
+            println(origin_x)
+            println(y)
+
+            var onebutton = Button(this)
+            onebutton.tag = i
+            onebutton.text = i.toString()
+            onelayout.addView(onebutton)
+
+            val lp = onebutton.getLayoutParams() as ConstraintLayout.LayoutParams
+            lp.height = 200
+            lp.width = 200
+            lp.topToTop = onelayout.top
+            lp.leftToLeft = onelayout.left
+            lp.topMargin = y
+            lp.leftMargin = origin_x
+            if (i == 10)
+            {
+                lp.leftMargin = origin_x + 230
+                onebutton.tag = 0
+                onebutton.text = "0"
+            }
+            onebutton.layoutParams = lp
+
+
+            onebutton.setOnClickListener {
+                println(onebutton.getTag())
+            }
+
+            if (i % 3 == 0)
+            {
+                origin_x = 30
+                y += 230
+            }
+            else
+            {
+                origin_x += 230
+            }
+        }
     }
 }
